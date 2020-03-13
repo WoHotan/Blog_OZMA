80a27a54f6e02b5b2395ad172f28e1a565f50a7a
温勋庆 <wenxunqing@wenxunqingde-Mac-mini.local>
2020-03-13 23:47:34 +0800

    定制admin后台

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Post, Category, Tag
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
+    fields = ['title', 'body', 'excerpt', 'categoty', 'tags']
 
-admin.site.register(Post)
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
