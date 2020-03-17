f7bf65ad8b8e06c351f9bf7d8e2f11fdf2736edc
温勋庆 <wenxunqing@wenxunqingde-Mac-mini.local>
2020-03-17 21:15:30 +0800

    设计文章详情页的URL

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 from django.utils import timezone
 # Create your models here.
 class Category(models.Model):
@@ -84,6 +85,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    # 自定义get_absolute_url 方法
+    # 从django.urls 中导入reverse函数
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
 
     def save(self, *args, **kwargs):
         self.modified_time = timezone.now()
